4ffbbe66219de65ff74df8e354d75a6f7bb9c6a9
Shivanshu Patel <shivanshuit914@gmail.com>
2022-09-30 19:52:24 +0100

    fix: linting

--- app/document/content/application-details.js
+++ app/document/content/application-details.js
@@ -18,16 +18,16 @@ const eligibility = (whichSpecies) => {
 const applicationDetails = (data) => {
   return {
     stack: [
-      { text: 'You have applied for funding for a review.\n\n'},
-      { text: 'By applying for this funding you’ve entered into an agreement with the Rural Payments Agency (RPA). \n\n'},
-      { text: 'The agreement will be governed by the agreed terms and conditions. \n\n', link: '#'},
-      { text: 'Agreement summary \n\n', style: 'subheader'},
+      { text: 'You have applied for funding for a review.\n\n' },
+      { text: 'By applying for this funding you’ve entered into an agreement with the Rural Payments Agency (RPA). \n\n' },
+      { text: 'The agreement will be governed by the agreed terms and conditions. \n\n', link: '#' },
+      { text: 'Agreement summary \n\n', style: 'subheader' },
       { text: `Agreement number: ${data.reference} \n\n` },
       { text: `Agreement holder: ${data.user.farmerName} - ${data.user.sbi} \n\n` },
       { text: `Agreement start date: ${data.startDate} \n\n` },
       { text: `Agreement end date: ${calculateEndDate(data.startDate, 6)} \n\n` },
       { text: `Type of livestock review: ${data.whichSpecies} \n\n` },
-      { text: `You must have a minimum number of animals on the date the vet visits your farm to do the review. do the review. You must have ${eligibility(data.whichSpecies)}\n\n` },
+      { text: `You must have a minimum number of animals on the date the vet visits your farm to do the review. do the review. You must have ${eligibility(data.whichSpecies)}\n\n` }
     ]
   }
 }
