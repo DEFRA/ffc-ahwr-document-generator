58ff51c571d97eee3ca298031d83ee8373e9f393
Shivanshu Patel <shivanshuit914@gmail.com>
2022-10-03 08:42:21 +0100

    feat: content changes (#3)
    * feat: content changes
    
    * bump: version
    
    * fix: linting
    
    * fix: tests

--- app/document/content/title.js
+++ app/document/content/title.js
@@ -1,8 +1,7 @@
 const title = () => {
   return {
     stack: [
-      { text: 'ANNUAL HEALTH AND WELFARE REVIEW', style: 'header', alignment: 'center' },
-      { text: 'PLACEHOLDER AGREEMENT DOCUMENT FOR TESTING ONLY \n\n\n', style: 'header', alignment: 'center' }
+      { text: 'Annual health and welfare review of livestock agreement summary', style: 'header', alignment: 'left' }
     ]
   }
 }
