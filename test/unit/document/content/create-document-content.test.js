bdba3662a66f1195ee9c00e688f375550a8a73e7
Steve Dickinson <32902665+steve-dickinson@users.noreply.github.com>
2022-10-06 11:57:08 +0100

    Add T&C (#5)
    * Add T&C

--- test/unit/document/content/create-document-content.test.js
+++ test/unit/document/content/create-document-content.test.js
@@ -12,8 +12,7 @@ describe('generate document content', () => {
 
   test('includes terms and condition', () => {
     const result = createContent(mockData)
-    expect(result[1].stack[2].text).toBe('The agreement will be governed by the agreed terms and conditions. \n\n')
-    expect(result[1].stack[2].link).toBe('#')
+    expect(result[1].stack[2].text).toBe('The agreement will be governed by the agreed terms and conditions. \n\n\n')
   })
 
   test('includes Agreement summary', () => {
@@ -34,7 +33,7 @@ describe('generate document content', () => {
 
   test('includes start date', () => {
     const result = createContent(mockData)
-    expect(result[1].stack[6].text).toBe(`Agreement start date: ${mockData.startDate} \n\n`)
+    expect(result[1].stack[6].text).toBe(`Agreement start date: ${moment(mockData.startDate).format('DD/MM/yyyy')} \n\n`)
   })
 
   test('includes Agreement end date', () => {
@@ -49,6 +48,6 @@ describe('generate document content', () => {
 
   test('includes Eligibility', () => {
     const result = createContent(mockData)
-    expect(result[1].stack[9].text).toBe('You must have a minimum number of animals on the date the vet visits your farm to do the review. do the review. You must have 11 or more beef cattle\n\n')
+    expect(result[1].stack[9].text).toBe('You must have a minimum number of animals on the date the vet visits your farm to do the review. You must have 11 or more beef cattle\n\n')
   })
 })
