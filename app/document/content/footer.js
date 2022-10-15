c6dc64e4edc99f447967ed998296b9ea40bc60ed
Shivanshu Patel <shivanshuit914@gmail.com>
2022-10-15 15:29:44 +0100

    fix: linting

--- app/document/content/footer.js
+++ app/document/content/footer.js
@@ -1,10 +1,9 @@
 const footer = (reference) => {
-    return {
-      stack: [
-        { text: `Date Generated: ${new Date().toLocaleString().split(', ')[0]}    Application Reference: ${reference}    Application Status: Agreement Offered`, style: 'footer' },
-      ]
-    }
+  return {
+    stack: [
+      { text: `Date Generated: ${new Date().toLocaleString().split(', ')[0]}    Application Reference: ${reference}    Application Status: Agreement Offered`, style: 'footer' },
+    ]
   }
+}
   
-  module.exports = footer
-  
+module.exports = footer
