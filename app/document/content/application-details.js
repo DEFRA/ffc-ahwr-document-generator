9026cb1b84c8edc5adc2d8366c2757966700d832
govTechSatish <80264942+govTechSatish@users.noreply.github.com>
2022-10-17 14:51:17 +0100

    updated content (#10)
    * updated content

--- app/document/content/application-details.js
+++ app/document/content/application-details.js
@@ -1,6 +1,5 @@
 const moment = require('moment')
 const config = require('../../config')
-const guidanceUri = `${config.applyServiceUri}/guidance-for-farmers`
 
 const calculateEndDate = (startDate, duration) => {
   return moment(startDate).add(duration, 'months').format('DD/MM/YYYY')
@@ -34,8 +33,7 @@ const applicationDetails = (data) => {
         ]
       },
       { text: `You must have a minimum number of animals on the date the vet visits your farm to do the review. You must have ${eligibility(data.whichSpecies)}.\n\n` },
-      { text: 'Terms and conditions\n\n', link: config.termsAndConditionsUrl, decoration: 'underline', color: '#1D70B8' },
-      { text: 'Guidance\n\n', link: guidanceUri, decoration: 'underline', color: '#1D70B8' }
+      { text: 'Terms and conditions\n\n', link: config.termsAndConditionsUrl, decoration: 'underline', color: '#1D70B8' }
     ]
   }
 }
