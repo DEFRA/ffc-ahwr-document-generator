716fb3b2b0be1b97676e66d987012ac1ab71f22d
liamwilson-kainos <liam.wilson@kainos.com>
2023-04-03 11:17:16 +0100

    Added JSON logging

--- app/messaging/process-document-request.js
+++ app/messaging/process-document-request.js
@@ -6,14 +6,14 @@ const processDocumentRequest = async (message, receiver) => {
   try {
     const messageBody = message.body
     if (validateDocumentRequest(messageBody)) {
-      console.log('Received document generation request', messageBody)
+      console.log('Received document generation request', JSON.stringify(messageBody))
       await generateDocument(messageBody)
       await sendFarmerApplicationEmail(messageBody)
       await receiver.completeMessage(message)
     }
   } catch (err) {
     await receiver.deadLetterMessage(message)
-    console.error('Unable to document generation request:', err)
+    console.error('Unable to document generation request:', err.message)
   }
 }
 
