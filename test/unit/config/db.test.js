d0a5c1b1d37d05719574fff35c38daff712632f2
Marcin Mogiela <marcinmo@kainos.com>
2023-06-13 16:22:24 +0200

    add beforeConnect hook test

---
+++ test/unit/config/db.test.js
@@ -0,0 +1,42 @@
+const dbConfig = require('../../../app/config/db')
+
+const MOCK_TOKEN = 'mockedAccessToken'
+
+jest.mock('@azure/identity', () => {
+  class MockDefaultAzureCredential {
+    async getToken () {
+      return { token: MOCK_TOKEN }
+    }
+  }
+
+  return {
+    DefaultAzureCredential: MockDefaultAzureCredential
+  }
+})
+
+describe('beforeConnect hook', () => {
+  test('should modify password in production environment', async () => {
+    const cfg = {
+      password: 'originalPassword'
+    }
+
+    process.env.NODE_ENV = 'production'
+
+    await dbConfig.development.hooks.beforeConnect(cfg)
+
+    expect(cfg.password).not.toBe('originalPassword')
+    expect(cfg.password).toEqual(MOCK_TOKEN)
+  })
+
+  test('should not modify password in non-production environment', async () => {
+    const cfg = {
+      password: 'originalPassword'
+    }
+
+    process.env.NODE_ENV = 'development'
+
+    await dbConfig.development.hooks.beforeConnect(cfg)
+
+    expect(cfg.password).toBe('originalPassword')
+  })
+})
