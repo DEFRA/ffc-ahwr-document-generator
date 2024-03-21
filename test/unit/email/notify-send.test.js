8067deb3906c948ca816dc0b6ed9acb709fca68b
elamin.fadlalla <elamin.fadlalla@capgemini.com>
2024-03-21 15:00:32 +0000

    add more test coverage for sendFarmerApplicationEmail

--- test/unit/email/notify-send.test.js
+++ test/unit/email/notify-send.test.js
@@ -38,6 +38,16 @@ describe('notify send email messages', () => {
     expect(response).toEqual(true)
   })
 
+  test('send farmer application email - successful email send when org email does not exist', async () => {
+    notifyClient.prepareUpload.mockReturnValue(Buffer.from('test').toString('base64'))
+    notifyClient.sendEmail.mockResolvedValue({ data: { id: notifyResponseId } })
+    const response = await sendFarmerApplicationEmail(mockData, Buffer.from('test').toString('base64'))
+    expect(consoleLog).toHaveBeenNthCalledWith(1, `File contents for ${mockDocumentRequest.whichSpecies}/${mockUser.sbi}/${mockDocumentRequest.reference}.pdf downloaded`)
+    expect(consoleLog).toHaveBeenNthCalledWith(3, `Received email to send to ${mockUser.email} for ${mockDocumentRequest.reference}`)
+    expect(consoleLog).toHaveBeenNthCalledWith(5, `Email sent to ${mockUser.email} for ${mockDocumentRequest.reference}`)
+    expect(response).toEqual(true)
+  })
+
   test('send farmer application email - error raised', async () => {
     notifyClient.prepareUpload.mockReturnValue(Buffer.from('test').toString('base64'))
     notifyClient.sendEmail.mockImplementation(() => { throw new Error() })
