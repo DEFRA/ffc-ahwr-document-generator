62ee820858f9ac1a2522be87dd691e2fdc25b8cb
Afshin <147500504+Afshin-Karamifar-Capgemini@users.noreply.github.com>
2024-05-24 13:41:27 +0100

    372390 - PDF content for apply submission has been updated (#58)
    * 372390 - PDF content for apply submition has been updated
    
    * lint fix

--- app/document/content/title.js
+++ app/document/content/title.js
@@ -1,13 +1,15 @@
 const path = require('path')
-const config = require('../../config')
+const { endemics, applyServiceUri } = require('../../config')
 const imagePath = path.join(__dirname, '../', 'images')
 const { millimetresToPoints } = require('../conversion')
 
 const title = () => {
+  const titleEndemicsOn = 'Agreement summary: get funding to improve animal health and welfare'
+  const titleEndemicsOff = 'Annual health and welfare review of livestock agreement summary'
   return {
     stack: [
-      { image: `${imagePath}/logo.jpg`, fit: [millimetresToPoints(200), millimetresToPoints(25)], style: 'logo', link: config.applyServiceUri },
-      { text: 'Annual health and welfare review of livestock agreement summary', style: 'header', alignment: 'left' }
+      { image: `${imagePath}/logo.jpg`, fit: [millimetresToPoints(200), millimetresToPoints(25)], style: 'logo', link: applyServiceUri },
+      { text: endemics.enabled ? titleEndemicsOn : titleEndemicsOff, style: 'header', alignment: 'left' }
     ]
   }
 }
