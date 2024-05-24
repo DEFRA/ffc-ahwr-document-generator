2175d4a4c82ccb7bd29b3b82676a4cb6f24c5f56
Afshin-Karamifar-Capgemini <afshin.karamifar@capgemini.com>
2024-05-24 12:48:58 +0100

    372390 - PDF content for apply submition has been updated

--- app/document/content/application-details.js
+++ app/document/content/application-details.js
@@ -1,5 +1,5 @@
 const moment = require('moment')
-const config = require('../../config')
+const { endemics, termsAndConditionsUrl, applyServiceUri } = require('../../config')
 
 const calculateEndDate = (startDate, duration) => {
   return moment(startDate).add(duration, 'months').format('D MMMM YYYY')
@@ -17,11 +17,48 @@ const eligibility = (whichSpecies) => {
 }
 
 const applicationDetails = (data) => {
-  return {
+  const applicationDetailsEndemicsOn = {
+    stack: [
+      { text: 'You have applied for funding for:', margin: [0, 10, 0, 6] },
+      {
+        ul: [
+          { text: 'animal health and welfare reviews', link: '#', decoration: 'underline', color: '#1D70B8', margin: [15, 0, 0, 5] },
+          { text: 'endemic disease follow-ups', link: '#', decoration: 'underline', color: '#1D70B8', margin: [15, 0, 0, 5] }
+        ]
+      },
+      { text: ['By applying for this ', { text: 'funding', link: '#', decoration: 'underline', color: '#1D70B8' }, { text: ' you have entered into an agreement with the Rural Payments Agency.' }], margin: [0, 20, 0, 10] },
+      { text: [{ text: 'The agreement will be governed by the ' }, { text: 'terms and conditions.', link: termsAndConditionsUrl, decoration: 'underline', color: '#1D70B8' }] },
+      { text: 'Agreement details', style: 'subheader', margin: [0, 20, 0, 7] },
+      {
+        ul: [
+          { text: `Agreement number: ${data.reference}`, margin: [15, 0, 0, 5] },
+          { text: `Agreement holder: ${data.name} - ${data.sbi}`, margin: [15, 0, 0, 5] },
+          { text: `Agreement start date: ${moment(data.startDate).format('D MMMM YYYY')} `, margin: [15, 0, 0, 5] },
+          { text: `Agreement end date: ${calculateEndDate(data.startDate, 6)}`, margin: [15, 0, 0, 0] }
+        ]
+      },
+      { text: 'Important requirements', style: 'subheader', margin: [0, 20, 0, 7] },
+      { text: 'You must:', margin: [0, 0, 0, 6] },
+      {
+        ul: [
+          { text: 'do all your reviews and follow-ups on the same species', margin: [15, 0, 0, 5] },
+          { text: ['have the ', { text: 'minimum number of the livestock', link: '#', decoration: 'underline', color: '#1D70B8' }, ' each time you do a review or follow-up'], margin: [15, 0, 0, 5] },
+          { text: ['follow the rules for ', { text: 'timing of reviews and follow-ups', link: '#', decoration: 'underline', color: '#1D70B8' }], margin: [15, 0, 0, 0] }
+        ]
+      },
+      { text: 'Guidance', style: 'subheader', margin: [0, 20, 0, 7] },
+      {
+        ul: [
+          { text: 'Get finding to improve animal health and welfare', link: '#', decoration: 'underline', color: '#1D70B8', margin: [15, 0, 0, 0] }
+        ]
+      }
+    ]
+  }
+  const applicationDetailsEndemicsOff = {
     stack: [
       { text: 'You have applied for funding for a review.\n\n' },
       { text: 'By applying for this funding you have entered into an agreement with the Rural Payments Agency (RPA). The agreement will be governed by the agreed ' },
-      { text: 'terms and conditions.\n\n', link: config.termsAndConditionsUrl, decoration: 'underline', color: '#1D70B8' },
+      { text: 'terms and conditions.\n\n', link: termsAndConditionsUrl, decoration: 'underline', color: '#1D70B8' },
       { text: 'Agreement summary \n\n', style: 'subheader' },
       {
         ul: [
@@ -35,11 +72,13 @@ const applicationDetails = (data) => {
       { text: `You must have a minimum number of animals on the date the vet visits your farm to do the review. You must have ${eligibility(data.whichSpecies)}.\n\n` },
       { text: 'More information\n\n', style: 'subheader' },
       { text: 'For information about what happens during a review and what you need to do, read the ' },
-      { text: 'guidance on how to apply for an annual health and welfare review of livestock.\n\n', link: `${config.applyServiceUri}/guidance-for-farmers`, decoration: 'underline', color: '#1D70B8' },
+      { text: 'guidance on how to apply for an annual health and welfare review of livestock.\n\n', link: `${applyServiceUri}/guidance-for-farmers`, decoration: 'underline', color: '#1D70B8' },
       { text: 'For information on how to claim, read the ' },
-      { text: 'guidance on how to claim funding for an annual health and welfare review of livestock.\n\n', link: `${config.applyServiceUri}/claim-guidance-for-farmers`, decoration: 'underline', color: '#1D70B8' }
+      { text: 'guidance on how to claim funding for an annual health and welfare review of livestock.\n\n', link: `${applyServiceUri}/claim-guidance-for-farmers`, decoration: 'underline', color: '#1D70B8' }
     ]
   }
+
+  return endemics.enabled ? applicationDetailsEndemicsOn : applicationDetailsEndemicsOff
 }
 
 module.exports = applicationDetails
--- test/unit/document/document-definition.test.js
+++ test/unit/document/document-definition.test.js
@@ -3,26 +3,59 @@ const createDocumentDefinition = require('../../../app/document/document-definit
 const mockData = require('../../mocks/data')
 const { A4 } = require('../../../app/document/page-sizes')
 const moment = require('moment')
+const { setEndemicsEnabled } = require('../../mocks/config')
 
 describe('get document definition', () => {
-  test('includes A4 paper size', () => {
-    const result = createDocumentDefinition(mockData)
-    expect(result.pageSize).toBe(A4)
-  })
+  describe('Endemics On', () => {
+    beforeEach(() => {
+      jest.resetAllMocks()
+      setEndemicsEnabled(false)
+    })
+    test('includes A4 paper size', () => {
+      const result = createDocumentDefinition(mockData)
+      expect(result.pageSize).toBe(A4)
+    })
 
-  test('includes all defined styles', () => {
-    const result = createDocumentDefinition(mockData)
-    expect(result.styles).toStrictEqual(styles)
-  })
+    test('includes all defined styles', () => {
+      const result = createDocumentDefinition(mockData)
+      expect(result.styles).toStrictEqual(styles)
+    })
 
-  test('included footer', () => {
-    const result = createDocumentDefinition(mockData)
-    expect(result.footer.stack[0].text).toBe(`Date Generated: ${moment(new Date()).format('DD/MM/YYYY')}    Application Reference: ${mockData.reference}    Application Status: Agreement Offered`)
-    expect(result.footer.stack[0].style).toBe('footer')
-  })
+    test('included footer', () => {
+      const result = createDocumentDefinition(mockData)
+      expect(result.footer.stack[0].text).toBe(`Date Generated: ${moment(new Date()).format('DD/MM/YYYY')}    Application Reference: ${mockData.reference}    Application Status: Agreement Offered`)
+      expect(result.footer.stack[0].style).toBe('footer')
+    })
 
-  test('sets default style as default style', () => {
-    const result = createDocumentDefinition(mockData)
-    expect(result.defaultStyle).toBe(styles.default)
+    test('sets default style as default style', () => {
+      const result = createDocumentDefinition(mockData)
+      expect(result.defaultStyle).toBe(styles.default)
+    })
+  })
+  describe('Endemics Off', () => {
+    beforeEach(() => {
+      jest.resetAllMocks()
+      setEndemicsEnabled(true)
+    })
+    test('includes A4 paper size', () => {
+      const result = createDocumentDefinition(mockData)
+      expect(result.pageSize).toBe(A4)
+    })
+
+    test('includes all defined styles', () => {
+      const result = createDocumentDefinition(mockData)
+      expect(result.styles).toStrictEqual(styles)
+    })
+
+    test('included footer', () => {
+      const result = createDocumentDefinition(mockData)
+      expect(result.footer.stack[0].text).toBe(`Date Generated: ${moment(new Date()).format('DD/MM/YYYY')}    Application Reference: ${mockData.reference}    Application Status: Agreement Offered`)
+      expect(result.footer.stack[0].style).toBe('footer')
+    })
+
+    test('sets default style as default style', () => {
+      const result = createDocumentDefinition(mockData)
+      expect(result.defaultStyle).toBe(styles.default)
+    })
   })
 })
