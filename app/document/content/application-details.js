7b1f242402201fc68de28f0a20e42df787506d89
JulieLev <JulieLev@users.noreply.github.com>
2024-06-08 01:35:24 +0100

    added links to application-details

--- app/document/content/application-details.js
+++ app/document/content/application-details.js
@@ -22,8 +22,8 @@ const applicationDetails = (data) => {
       { text: 'You have applied for funding for:', margin: [0, 10, 0, 6] },
       {
         ul: [
-          { text: 'animal health and welfare reviews', link: '#', decoration: 'underline', color: '#1D70B8', margin: [15, 0, 0, 5] },
-          { text: 'endemic disease follow-ups', link: '#', decoration: 'underline', color: '#1D70B8', margin: [15, 0, 0, 5] }
+          { text: 'animal health and welfare reviews', link: 'https://www.gov.uk/guidance/farmers-how-to-apply-for-funding-to-improve-animal-health-and-welfare#animal-health-and-welfare-review', decoration: 'underline', color: '#1D70B8', margin: [15, 0, 0, 5] },
+          { text: 'endemic disease follow-ups', link: 'https://www.gov.uk/guidance/farmers-how-to-apply-for-funding-to-improve-animal-health-and-welfare#endemic-disease-follow-up', decoration: 'underline', color: '#1D70B8', margin: [15, 0, 0, 5] }
         ]
       },
       { text: ['By applying for this ', { text: 'funding', link: '#', decoration: 'underline', color: '#1D70B8' }, { text: ' you have entered into an agreement with the Rural Payments Agency.' }], margin: [0, 20, 0, 10] },
@@ -42,14 +42,14 @@ const applicationDetails = (data) => {
       {
         ul: [
           { text: 'do all your reviews and follow-ups on the same species', margin: [15, 0, 0, 5] },
-          { text: ['have the ', { text: 'minimum number of the livestock', link: '#', decoration: 'underline', color: '#1D70B8' }, ' each time you do a review or follow-up'], margin: [15, 0, 0, 5] },
-          { text: ['follow the rules for ', { text: 'timing of reviews and follow-ups', link: '#', decoration: 'underline', color: '#1D70B8' }], margin: [15, 0, 0, 0] }
+          { text: ['have the ', { text: 'minimum number of the livestock', link: 'https://www.gov.uk/guidance/farmers-how-to-apply-for-funding-to-improve-animal-health-and-welfare#who-can-get-funding', decoration: 'underline', color: '#1D70B8' }, ' each time you do a review or follow-up'], margin: [15, 0, 0, 5] },
+          { text: ['follow the rules for ', { text: 'timing of reviews and follow-ups', link: 'https://www.gov.uk/guidance/farmers-how-to-apply-for-funding-to-improve-animal-health-and-welfare#timing-of-reviews-and-follow-ups', decoration: 'underline', color: '#1D70B8' }], margin: [15, 0, 0, 0] }
         ]
       },
       { text: 'Guidance', style: 'subheader', margin: [0, 20, 0, 7] },
       {
         ul: [
-          { text: 'Get funding to improve animal health and welfare', link: '#', decoration: 'underline', color: '#1D70B8', margin: [15, 0, 0, 0] }
+          { text: 'Get funding to improve animal health and welfare', link: 'https://www.gov.uk/government/collections/funding-to-improve-animal-health-and-welfare-guidance-for-farmers-and-vets', decoration: 'underline', color: '#1D70B8', margin: [15, 0, 0, 0] }
         ]
       }
     ]
--- test/unit/document/content/create-document-content.test.js
+++ test/unit/document/content/create-document-content.test.js
@@ -2,67 +2,73 @@ const moment = require('moment')
 const config = require('../../../../app/config')
 const createContent = require('../../../../app/document/content')
 const mockData = require('../../../mocks/data')
+const { setEndemicsEnabled } = require('../../../mocks/config')
 
 describe('generate document content', () => {
-  let result
-  beforeEach(() => {
-    result = createContent(mockData)
-  })
+  describe('Endemics Off', () => {
+    let result
 
-  test('includes header and logo', () => {
-    expect(result[0].stack[0].style).toBe('logo')
-    expect(result[0].stack[1].text).toBe('Annual health and welfare review of livestock agreement summary')
-    expect(result[0].stack[1].style).toBe('header')
-    expect(result[0].stack[1].alignment).toBe('left')
-  })
+    beforeEach(() => {
+      jest.resetAllMocks()
+      setEndemicsEnabled(false)
+      result = createContent(mockData)
+    })
 
-  test('includes agreement text', () => {
-    expect(result[1].stack[0].text).toBe('You have applied for funding for a review.\n\n')
-    expect(result[1].stack[1].text).toBe('By applying for this funding you have entered into an agreement with the Rural Payments Agency (RPA). The agreement will be governed by the agreed ')
-  })
+    test('includes header and logo', () => {
+      expect(result[0].stack[0].style).toBe('logo')
+      expect(result[0].stack[1].text).toBe('Annual health and welfare review of livestock agreement summary')
+      expect(result[0].stack[1].style).toBe('header')
+      expect(result[0].stack[1].alignment).toBe('left')
+    })
 
-  test('includes terms and conditions link', () => {
-    expect(result[1].stack[2].text).toBe('terms and conditions.\n\n')
-    expect(result[1].stack[2].link).toBe(config.termsAndConditionsUrl)
-  })
+    test('includes agreement text', () => {
+      expect(result[1].stack[0].text).toBe('You have applied for funding for a review.\n\n')
+      expect(result[1].stack[1].text).toBe('By applying for this funding you have entered into an agreement with the Rural Payments Agency (RPA). The agreement will be governed by the agreed ')
+    })
 
-  test('includes agreement summary sub header', () => {
-    expect(result[1].stack[3].text).toBe('Agreement summary \n\n')
-    expect(result[1].stack[3].style).toBe('subheader')
-  })
+    test('includes terms and conditions link', () => {
+      expect(result[1].stack[2].text).toBe('terms and conditions.\n\n')
+      expect(result[1].stack[2].link).toBe(config.termsAndConditionsUrl)
+    })
 
-  test('includes Agreement details', () => {
-    expect(result[1].stack[4].ul[0]).toBe(`Agreement number: ${mockData.reference} \n\n`)
-    expect(result[1].stack[4].ul[1]).toBe(`Agreement holder: ${mockData.name} - ${mockData.sbi} \n\n`)
-    expect(result[1].stack[4].ul[2]).toBe(`Agreement start date: ${moment(mockData.startDate).format('D MMMM YYYY')} \n\n`)
-    expect(result[1].stack[4].ul[3]).toBe(`Agreement end date: ${moment(mockData.startDate).add(6, 'M').format('D MMMM YYYY')} \n\n`)
-    expect(result[1].stack[4].ul[4]).toBe(`Type of livestock review: ${mockData.whichSpecies} \n\n`)
-  })
+    test('includes agreement summary sub header', () => {
+      expect(result[1].stack[3].text).toBe('Agreement summary \n\n')
+      expect(result[1].stack[3].style).toBe('subheader')
+    })
 
-  test('includes Eligibility', () => {
-    expect(result[1].stack[5].text).toBe('You must have a minimum number of animals on the date the vet visits your farm to do the review. You must have 11 or more beef cattle.\n\n')
-  })
+    test('includes Agreement details', () => {
+      expect(result[1].stack[4].ul[0]).toBe(`Agreement number: ${mockData.reference} \n\n`)
+      expect(result[1].stack[4].ul[1]).toBe(`Agreement holder: ${mockData.name} - ${mockData.sbi} \n\n`)
+      expect(result[1].stack[4].ul[2]).toBe(`Agreement start date: ${moment(mockData.startDate).format('D MMMM YYYY')} \n\n`)
+      expect(result[1].stack[4].ul[3]).toBe(`Agreement end date: ${moment(mockData.startDate).add(6, 'M').format('D MMMM YYYY')} \n\n`)
+      expect(result[1].stack[4].ul[4]).toBe(`Type of livestock review: ${mockData.whichSpecies} \n\n`)
+    })
 
-  test('includes more information sub header', () => {
-    expect(result[1].stack[6].text).toBe('More information\n\n')
-    expect(result[1].stack[6].style).toBe('subheader')
-  })
+    test('includes Eligibility', () => {
+      expect(result[1].stack[5].text).toBe('You must have a minimum number of animals on the date the vet visits your farm to do the review. You must have 11 or more beef cattle.\n\n')
+    })
 
-  test('includes application guidance text', () => {
-    expect(result[1].stack[7].text).toBe('For information about what happens during a review and what you need to do, read the ')
-  })
+    test('includes more information sub header', () => {
+      expect(result[1].stack[6].text).toBe('More information\n\n')
+      expect(result[1].stack[6].style).toBe('subheader')
+    })
 
-  test('includes application guidance link', () => {
-    expect(result[1].stack[8].text).toBe('guidance on how to apply for an annual health and welfare review of livestock.\n\n')
-    expect(result[1].stack[8].link).toBe(`${config.applyServiceUri}/guidance-for-farmers`)
-  })
+    test('includes application guidance text', () => {
+      expect(result[1].stack[7].text).toBe('For information about what happens during a review and what you need to do, read the ')
+    })
 
-  test('includes claim guidance text', () => {
-    expect(result[1].stack[9].text).toBe('For information on how to claim, read the ')
-  })
+    test('includes application guidance link', () => {
+      expect(result[1].stack[8].text).toBe('guidance on how to apply for an annual health and welfare review of livestock.\n\n')
+      expect(result[1].stack[8].link).toBe(`${config.applyServiceUri}/guidance-for-farmers`)
+    })
 
-  test('includes claim guidance link', () => {
-    expect(result[1].stack[10].text).toBe('guidance on how to claim funding for an annual health and welfare review of livestock.\n\n')
-    expect(result[1].stack[10].link).toBe(`${config.applyServiceUri}/claim-guidance-for-farmers`)
+    test('includes claim guidance text', () => {
+      expect(result[1].stack[9].text).toBe('For information on how to claim, read the ')
+    })
+
+    test('includes claim guidance link', () => {
+      expect(result[1].stack[10].text).toBe('guidance on how to claim funding for an annual health and welfare review of livestock.\n\n')
+      expect(result[1].stack[10].link).toBe(`${config.applyServiceUri}/claim-guidance-for-farmers`)
+    })
   })
 })
