1a8b1c386b62b7366c433893952a36579282e93d
Ben Hope <bendef24@gmail.com>
2024-11-12 22:33:32 +0000

    feat(AHWR-202): fixed linting issue

--- app/email/sfd-client.js
+++ app/email/sfd-client.js
@@ -4,23 +4,23 @@ const validateSFDClaim = require('../messaging/submit-sfd-schema')
 const states = require('../messaging/states')
 
 const sendSFDEmail = async (templateId, email, personalisation) => {
-    const { personalisation: { reference, crn, sbi } } = personalisation
+  const { personalisation: { reference, crn, sbi } } = personalisation
 
-    let sfdMessage = {
-        crn: crn,
-        sbi: sbi,
-        agreementReference: reference,
-        notifyTemplateId: templateId,
-        emailAddress: email,
-        customParams: personalisation,
-        dateTime: new Date().toISOString()
-    }
+  const sfdMessage = {
+    crn: crn,
+    sbi: sbi,
+    agreementReference: reference,
+    notifyTemplateId: templateId,
+    emailAddress: email,
+    customParams: personalisation,
+    dateTime: new Date().toISOString()
+  }
 
-    if (validateSFDClaim(sfdMessage)) {
-        return await sendMessage(sfdMessage, sfdRequestMsgType, sfdMessageQueue)
-    } else {
-        return sendMessage({ applicationState: states.failed }, sfdRequestMsgType, sfdMessageQueue, { templateId })
-    }
+  if (validateSFDClaim(sfdMessage)) {
+    return await sendMessage(sfdMessage, sfdRequestMsgType, sfdMessageQueue)
+  } else {
+    return sendMessage({ applicationState: states.failed }, sfdRequestMsgType, sfdMessageQueue, { templateId })
+  }
 }
 
 module.exports = sendSFDEmail
