8f89f7ff50a97543af2760735308f0dbd0e2e886
Rob Catton <robmcatton@gmail.com>
2025-01-13 13:36:17 +0000

    Fixing test

--- test/unit/document/content/title.test.js
+++ test/unit/document/content/title.test.js
@@ -8,13 +8,13 @@ jest.mock('../../../../app/config', () => ({
 }))
 
 describe('title', () => {
-  test('it build an object to use in the page content', () => {
+  test('it builds an object to use in the page content', () => {
     expect(title()).toEqual({
       stack: [
         {
           fit: [567, 70.875],
           image:
-            '/Users/robertcatton/repos/VetsVisits/ffc-ahwr-document-generator/app/document/images/logo.jpg',
+            '/home/node/app/document/images/logo.jpg',
           link: 'test-uri',
           style: 'logo'
         },
