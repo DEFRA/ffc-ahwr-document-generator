29d4a04792ce89c10f3dc9f0d8b7f66f3c468e9b
Ben Hope <bendef24@gmail.com>
2025-07-16 15:50:27 +0100

    feat(AHWR-1067): Implement retention policy, added entry point with empty implementation.

--- app/plugins/router.js
+++ app/plugins/router.js
@@ -1,7 +1,8 @@
 import { healthyRoute } from '../routes/healthy.js'
 import { healthzRoute } from '../routes/healthz.js'
+import { piiRedactRequestHandlers } from '../routes/redact-pii.js'
 
-const routes = [healthyRoute, healthzRoute]
+const routes = [healthyRoute, healthzRoute, ...piiRedactRequestHandlers]
 
 export const routerPlugin = {
   plugin: {
