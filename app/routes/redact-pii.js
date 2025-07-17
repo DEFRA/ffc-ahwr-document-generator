16a8b9b2be27302b8f8de2f6eab192afe8c857eb
Ben Hope <bendef24@gmail.com>
2025-07-17 16:13:27 +0100

    feat(AHWR-1067): Implement retention policy, added entry point with empty implementation.

--- app/routes/redact-pii.js
+++ app/routes/redact-pii.js
@@ -1,10 +1,12 @@
+import HttpStatus from "http-status-codes";
+
 export const redactPiiRequestHandlers = [
   {
     method: 'POST',
     path: '/api/redact/pii',
     handler: async (request, h) => {
       request.logger.info(`Request for redact PII received, agreementsToRedact: ${request.body}`)
-      return h.response().code(200)
+      return h.response().code(HttpStatus.OK)
     }
   }
 ]
