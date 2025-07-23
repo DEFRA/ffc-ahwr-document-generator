89349636792825f164afecbbfaa798eb410ad06f
Ben Hope <bendef24@gmail.com>
2025-07-23 10:54:58 +0100

    feat(AHWR-1067): Implement retention policy, added implementation

--- app/repositories/document-log-repository.js
+++ app/repositories/document-log-repository.js
@@ -45,8 +45,9 @@ export const redactPII = async (applicationReference) => {
     Sequelize.literal(`'"${REDACT_PII_VALUES.REDACTED_FARMER_NAME}"'`)
   )
 
+  // TODO 1067 add logging to say what was updated? 
   // eslint-disable-next-line no-unused-vars
-  // const [_, updates] = await models.application.update(
+  // const [_, updates] = await models.documentLog.update(
   await models.documentLog.update(
     { data },
     {
@@ -54,30 +55,4 @@ export const redactPII = async (applicationReference) => {
       returning: true
     }
   )
-
-  // TODO 1067 add later
-  // const [updatedRecord] = updates
-  // const { updatedAt, data: { organisation: { sbi } } } = updatedRecord.dataValues
-
-  // const eventData = {
-  //   applicationReference: reference,
-  //   reference,
-  //   updatedProperty,
-  //   newValue,
-  //   oldValue,
-  //   note
-  // }
-  // const type = `application-${updatedProperty}`
-  // await claimDataUpdateEvent(eventData, type, user, updatedAt, sbi)
-
-  // await buildData.models.claim_update_history.create({
-  //   applicationReference: reference,
-  //   reference,
-  //   note,
-  //   updatedProperty,
-  //   newValue,
-  //   oldValue,
-  //   eventType: type,
-  //   createdBy: user
-  // })
 }
